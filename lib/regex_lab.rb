45de56508225d512d2221b6f0689f652d8472d24
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-11 01:58:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  /[aeiou]
+  word.match(//)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/(ing)/)
 end
 
 def words_five_letters_long(text)
